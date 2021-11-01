bf8b1c22a9e3844916670fdb659d2ff0c9ad7065
Maikoncarlos <maikonsaturno@gmail.com>
2021-11-01 16:27:51 -0300

    projetos criados back e front - Spring React

---
+++ backend/src/main/java/com/github/Maikoncarlos/Spring/React/SpringReactApplication.java
@@ -0,0 +1,13 @@
+package com.github.Maikoncarlos.Spring.React;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/Maikoncarlos/Spring/React/SpringReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.Maikoncarlos.Spring.React;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
