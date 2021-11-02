05cafa72b8329947e6fb96de80d3d8904fe74d86
Maikoncarlos <maikonsaturno@gmail.com>
2021-11-01 23:45:30 -0300

    basic static component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,18 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import DataTable from "components/DataTable";
+
 function App() {
-  return(
-   <div>
-    <h1 className="text-primary">Olá Spring React</h1>
-  </div>);
+  return (
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá Spring React</h1>
+        <DataTable/>
+      </div>
+      <Footer/>
+    </>
+  );
 }
 
 export default App;
