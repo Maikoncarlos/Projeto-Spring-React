545ed4e6250b6d97cf3600b0ec3842797bae9dca
Maikoncarlos <maikonsaturno@gmail.com>
2021-11-07 12:36:50 -0300

    Barchart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -16,20 +16,14 @@ function DonutChart() {
   });
 
   useEffect(() => {
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-    .then((response) => {
-        const data = response.data as SaleSum[];
-        const myLabels = data.map((x) => x.sellerName);
-        const mySeries = data.map((x) => x.sum);
+    axios.get(`${BASE_URL}/sales/amount-by-seller`).then((response) => {
+      const data = response.data as SaleSum[];
+      const myLabels = data.map((x) => x.sellerName);
+      const mySeries = data.map((x) => x.sum);
 
-        setChartData({ labels: myLabels, series: mySeries });
+      setChartData({ labels: myLabels, series: mySeries });
     });
   }, []);
-
-  //const mockData = {
-  //  series: [477138, 499928, 444867, 220426, 473088],
-  // labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
-  //}
 
   const options = {
     legend: {
