c09dd5beed4e74fb4d607f44eed04e784a5d4171
Maikoncarlos <maikonsaturno@gmail.com>
2021-11-07 22:34:11 -0300

    mikam@DESKTOP-TEQL3QC MINGW64 ~/workspace/ws-devsuperior/projeto-sds (master)

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,10 +1,33 @@
+import { Seller } from "./seller";
+
+export type Sale = {
+  id: number;
+  visited: number;
+  deals: number;
+  amount: number;
+  date: string;
+  seller: Seller;
+};
+
+export type SalePage = {
+  content?: Sale[];
+  totalPages: number;
+  totalElements: number;
+  last: boolean;
+  number: number;
+  size?: number;
+  numberOfElements?: number;
+  first: boolean;
+  empty?: boolean;
+};
+
 export type SaleSum = {
-    sellerName: string,
-    sum: number
-}
+  sellerName: string;
+  sum: number;
+};
 
 export type SaleSucess = {
-    sellerName: string;
-    visited: number;
-    deals: number;
-}
+  sellerName: string;
+  visited: number;
+  deals: number;
+};
